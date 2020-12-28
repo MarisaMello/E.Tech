ed83c7d1ebe2e759d0ffb717f7df17b12c690b77
Marisa de  Mello <marisamcavalcanti@hotmail.com>
2020-12-28 15:40:25 -0300

    Accordion

--- src/main/java/br/com/treinamento/etech/commons/AppiumRobot.java
+++ src/main/java/br/com/treinamento/etech/commons/AppiumRobot.java
@@ -1,10 +1,16 @@
 package br.com.treinamento.etech.commons;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class AppiumRobot extends BaseTest{
 
     public void clicarPorTexto(String valor){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
        driver.findElement(By.xpath("//*[@text='"+valor+"']")).click();
     }
 
---
+++ target/test-classes/features/Accordion.feature
@@ -0,0 +1,12 @@
+#language: pt
+#enconding: UTF-8
+#Author: Marisa de Mello
+
+  Funcionalidade: Accordion
+
+    @mobile @accordion
+    Cenario: validar accordion
+    Dado que esteja na aplicação
+    E toco no botão "Accordion" 
+    Quando toco no botão "Opção 1" 
+    Então visualizo o texto na tela "Esta é a descrição da opção 1"
